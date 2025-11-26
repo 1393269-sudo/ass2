3bb7a87533b89797c981bdf568ec0a23c88c789f
sam <1393269@cityplym.ac.uk>
2025-11-26 11:44:36 +0000

    created a simple menu to make nodes

---
+++ src/main/java/Main/menu.java
@@ -0,0 +1,36 @@
+package Main;
+
+import  Models.Resource;
+import Repository.store;
+
+import java.util.Scanner;
+
+public class menu {
+    public static void main(String[] args){
+
+        store store = new store();
+        Scanner sc = new Scanner(System.in);
+
+        while (true){
+            System.out.println("1. Add Node");
+            System.out.println("2. Test count node");
+
+            String option = sc.nextLine();
+
+            switch (option) {
+                case "1":
+                    System.out.println("Enter node name");
+                    String name = sc.nextLine();
+
+                    System.out.println("Enter Description");
+                    String description = sc.next();
+
+                case "2":
+                    store.countResources();
+                    break;
+                    default:
+                        System.out.println("Invalid option");
+            }
+        }
+    }
+}
